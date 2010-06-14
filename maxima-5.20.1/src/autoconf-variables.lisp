84a3a30dadd596e696f35a90416b90268e46152c
G.O.Passmore <g.o.passmore@sms.ed.ac.uk>
2010-06-14 15:23:57 +0100

    Added Maxima into repo.

---
+++ maxima-5.20.1/src/autoconf-variables.lisp
@@ -0,0 +1,18 @@
+; -*- Lisp -*-
+(in-package :maxima)
+
+(defparameter *autoconf-variables-set* "@variables_set@")
+(defparameter *autoconf-prefix* "/Research/Programs/MyCode/rahd/maxima-5.20.1")
+(defparameter *autoconf-exec_prefix* "@expanded_exec_prefix@")
+(defparameter *autoconf-package* "maxima")
+(defparameter *autoconf-version* "5.20.1")
+(defparameter *autoconf-libdir* "@expanded_libdir@")
+(defparameter *autoconf-libexecdir* "@expanded_libexecdir@")
+(defparameter *autoconf-datadir* "@expanded_datadir@")
+(defparameter *autoconf-infodir* "@expanded_infodir@")
+(defparameter *autoconf-host* "@host@")
+(defparameter *autoconf-win32* "false")
+(defparameter *autoconf-ld-flags* "@LDFLAGS@")
+ 
+(defparameter *maxima-source-root* "/Research/Programs/MyCode/rahd/maxima-5.20.1")
+(defparameter *maxima-default-layout-autotools* "false")
